b215b94b2b6b677d393af7334104d3d05b635e22
Steven Berler <steven.berler@dreamhost.com>
2012-10-11 12:48:14 -0700

    handle arbitrary structures for bw notifications
    Adds the ability to handle an arbitrary number of nested dictionaries
    in the notification payload.  The keys in each nested dict are joined
    using '.' to produce the Counter name.

--- akanda/ceilometer/notifications.py
+++ akanda/ceilometer/notifications.py
@@ -36,21 +36,31 @@ class NetworkBandwidthNotification(plugin.NotificationBase):
                            for topic in conf.akanda_notification_topics)),
             ]
 
+    def _notifications_from_payload(self, payload):
+        """Given a notification message payload, returns (name, value) pairs
+        for all the counters that should be generated."""
+        for name in payload:
+            value = payload[name]
+            # FIXME (sberler): is there a better way than using type()?
+            if type(value) is dict:
+                for (subname, val) in self._notifications_from_payload(value):
+                    yield (name + '.' + subname, val)
+            else:
+                yield (name, value)
+
     def process_notification(self, message):
         LOG.info('akanda network notification %r', message)
 
-        # TODO (sberler): handle arbitrary nested dictionaries instead
-        # of just two levels.
-        for type_ in message['payload']:
-            for subtype in message['payload'][type_]:
-                yield counter.Counter(
-                    source='?',
-                    name='akanda.bandwidth:%s.%s' % (type_, subtype),
-                    type=counter.TYPE_DELTA,
-                    volume=message['payload'][type_][subtype],
-                    project_id=message['tenant_id'],
-                    timestamp=message['timestamp'],
-                    user_id=None,
-                    resource_id=None,
-                    resource_metadata={},
-                    )
+        payload = message['payload']
+        for (name, value) in self._notifications_from_payload(payload):
+            yield counter.Counter(
+                source='?',
+                name='akanda.bandwidth:%s' % name,
+                type=counter.TYPE_DELTA,
+                volume=value,
+                project_id=message['tenant_id'],
+                timestamp=message['timestamp'],
+                user_id=None,
+                resource_id=None,
+                resource_metadata={},
+                )
--- test/test_notifications.py
+++ test/test_notifications.py
@@ -17,15 +17,15 @@ BW_NOTIFICATION = {
     u'_context_user_id': u'1e3ce043029547f1a61c1996d1a531a2',
     u'event_type': u'akanda.bandwidth.used',
     u'message_id': u'dae6f69c-00e0-41c0-b371-41ec3b7f4451',
-    u'payload': {u'external': {u'packets_out': 1234,
-                               u'bytes_out': 22434,
-                               u'packets_in': 88,
-                               u'bytes_in': 89420,
+    u'payload': {u'external': {u'out.packets': 1234,
+                               u'out.bytes': 22434,
+                               u'in.packets': 88,
+                               u'in.bytes': 89420,
                                },
-                 u'internal': {u'packets_out': 4567,
-                               u'bytes_out': 82984,
-                               u'packets_in': 77,
-                               u'bytes_in': 98982,
+                 u'internal': {u'out.packets': 4567,
+                               u'out.bytes': 82984,
+                               u'in.packets': 77,
+                               u'in.bytes': 98982,
                                }
                  },
     u'priority': u'INFO',
@@ -33,6 +33,49 @@ BW_NOTIFICATION = {
     u'timestamp': u'2012-05-08 20:23:48.028195',
     }
 
+BW_NOTIFICATION_ALT = {
+    u'_context_auth_token': u'3d8b13de1b7d499587dfc69b77dc09c2',
+    u'_context_is_admin': True,
+    u'_context_project_id': u'7c150a59fe714e6f9263774af9688f0e',
+    u'_context_quota_class': None,
+    u'_context_read_deleted': u'no',
+    u'_context_remote_address': u'10.0.2.15',
+    u'_context_request_id': u'req-d68b36e0-9233-467f-9afb-d81435d64d66',
+    u'_context_roles': [u'admin'],
+    u'_context_timestamp': u'2012-05-08T20:23:41.425105',
+    u'_context_user_id': u'1e3ce043029547f1a61c1996d1a531a2',
+    u'event_type': u'akanda.bandwidth.used',
+    u'message_id': u'dae6f69c-00e0-41c0-b371-41ec3b7f4451',
+    u'payload': {u'external': {u'out': {u'packets': 1234,
+                                        u'bytes': 22434,
+                                        },
+                               u'in': {u'packets': 88,
+                                       u'bytes': 89420,
+                                       },
+                               },
+                 u'internal': {u'out': {u'packets': 4567,
+                                        u'bytes': 82984,
+                                        },
+                               u'in': {u'packets': 77,
+                                       u'bytes': 98982,
+                                       },
+                               },
+                 },
+    u'priority': u'INFO',
+    u'tenant_id': u'7c150a59fe714e6f9263774af9688f0e',
+    u'timestamp': u'2012-05-08 20:23:48.028195',
+    }
+
+EXPECTED_COUNTERS = {
+    'akanda.bandwidth:external.out.packets': 1234,
+    'akanda.bandwidth:external.out.bytes': 22434,
+    'akanda.bandwidth:external.in.packets': 88,
+    'akanda.bandwidth:external.in.bytes': 89420,
+    'akanda.bandwidth:internal.out.packets': 4567,
+    'akanda.bandwidth:internal.out.bytes': 82984,
+    'akanda.bandwidth:internal.in.packets': 77,
+    'akanda.bandwidth:internal.in.bytes': 98982,
+    }
 
 class TestNotifications(unittest.TestCase):
 
@@ -73,25 +116,29 @@ class TestNotifications(unittest.TestCase):
         assert got_exchange == expected_exchange
         assert got_topics == expected_topics
 
-    def test_process_notification(self):
+    def _check_notification(self, message, expected_counters):
         got = list(
-            self.bw_handler.process_notification(BW_NOTIFICATION)
+            self.bw_handler.process_notification(message)
             )
-        expected_counters = {
-            'akanda.bandwidth:external.packets_out': 1234,
-            'akanda.bandwidth:external.bytes_out': 22434,
-            'akanda.bandwidth:external.packets_in': 88,
-            'akanda.bandwidth:external.bytes_in': 89420,
-            'akanda.bandwidth:internal.packets_out': 4567,
-            'akanda.bandwidth:internal.bytes_out': 82984,
-            'akanda.bandwidth:internal.packets_in': 77,
-            'akanda.bandwidth:internal.bytes_in': 98982,
-            }
-
         assert len(got) == len(expected_counters)
         for name in expected_counters:
             counter = TestNotifications._find_counter(got, name)
             assert counter is not None
-            assert counter.volume == expected_counters[name]
-            assert counter.project_id == BW_NOTIFICATION['tenant_id']
-            assert counter.timestamp == BW_NOTIFICATION['timestamp']
+
+            got_volume = counter.volume
+            expected_volume = expected_counters[name]
+            assert got_volume == expected_volume
+
+            got_project_id = counter.project_id
+            expected_project_id = message['tenant_id']
+            assert got_project_id == expected_project_id
+
+            got_timestamp = counter.timestamp
+            expected_timestamp = message['timestamp']
+            assert got_timestamp == expected_timestamp
+
+    def test_process_notification(self):
+        self._check_notification(BW_NOTIFICATION, EXPECTED_COUNTERS)
+
+    def test_process_notification_alt(self):
+        self._check_notification(BW_NOTIFICATION_ALT, EXPECTED_COUNTERS)
